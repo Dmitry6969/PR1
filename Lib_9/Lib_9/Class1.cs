64603477bbe9e8b7505493076d0c6034242c7c31
Dmitry6969 <73423142+Dmitry6969@users.noreply.github.com>
2021-10-05 13:19:19 +0300

    Add files via upload

---
+++ Lib_9/Lib_9/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_9
+{
+    public class Class1
+    {
+        public static int SummLess8(List<int> numbers)//функция суммирования ряда чисел
+        {                               
+            int summ;//сумма
+            int n = numbers.Count();//количество элементов
+            int l = 0;//счетчик
+            List<int> numbers8 = new List<int>();//новый список для чисел меньше 8
+
+            do// отбор чисел меньше 8  в новый ряд
+            {
+                if (numbers[l] < 8)
+                {
+                    numbers8.Add(numbers[l]);
+                }
+                l++;
+
+            } while (l <= n - 1);
+
+            summ = numbers8.Sum();//суммирование
+            return summ;//возврат суммы
+
+        }
+
+    }
+}
